19dd19c1a6084e6dcbbbcdc0377c7314caf643fc
Vikash <dwivedi@Mac.lan>
2026-05-23 10:35:04 +0530

    Added one test case and integrate allure reporting as well

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,12 @@
 package base;
 
+import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners({AllureTestNg.class})
 public class BaseTest {
 
     protected WebDriver driver;
--- src/test/java/test/ui/LoginTest.java
+++ src/test/java/test/ui/LoginTest.java
@@ -1,23 +1,28 @@
 package test.ui;
 
 import base.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.DataProvider;
 import pages.loginPage;
 import org.testng.annotations.Test;
-
+@Epic("Login Module")
+@Feature("User Login")
 public class LoginTest extends BaseTest {
 
 
     @DataProvider(name = "getTestData")
     public Object[][] getLoginData() {
         return new Object[][] {
-                {"Admin", "admin123"},
-                {"Admin", "wrongPassword"}
+                {"Admin", "admin123"}
         };
     }
     @Test(dataProvider = "getTestData")
+    @Story("Verify login functionality with valid and invalid credentials")
+    @Description("This test verifies login functionality for multiple user scenarios")
     public void verifyLogin(String username, String password) {
-
         loginPage login = new loginPage(driver);
         login.login(username, password);
     }
